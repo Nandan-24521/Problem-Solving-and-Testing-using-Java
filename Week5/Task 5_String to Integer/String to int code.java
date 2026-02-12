55e3128b1306652432e78532a9a05b1988dbe8f5
Nandan-24521 <vtu24521@veltech.edu.in>
2026-02-12 09:47:57 +0530

    Create String to int code.java

---
+++ Week5/Task 5_String to Integer/String to int code.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int i = 0;
+        int n = s.length();
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i >= n) {
+            return 0;
+        }
+
+        int sign = 1;
+        if (s.charAt(i) == '+') {
+            i++;
+        } else if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+        
+        long result = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+
+            if (c < '0' || c > '9') {
+                break;
+            }
+            
+            int digit = c - '0';
+            result = result * 10 + digit;
+            
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            
+            i++;
+        }
+        
+        return (int)(sign * result);
+    }
+}
